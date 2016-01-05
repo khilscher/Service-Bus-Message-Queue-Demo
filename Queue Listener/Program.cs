4a6d13a6e0d7e219429105a611828a261636cc45
khilscher <kevin@venta.ca>
2016-01-04 20:00:41 -0700

    Initial commit

---
+++ Queue Listener/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.ServiceBus.Messaging;
+
+namespace Queue_Listener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queueName = "khqueue1";
+            //Copy & paste receiver SB Queue endpoint from Azure portal; append TransportType=Amqp to use AMQP
+            string connection = "Endpoint=sb://khtest1.servicebus.windows.net/;SharedAccessKeyName=Receiver;SharedAccessKey=vIqa5vlo9+c0YGxu+9xadEQ1aMUdwxgrcu2YCOqJNFw=;TransportType=Amqp";
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+            QueueClient queue = factory.CreateQueueClient(queueName);
+            while (true)
+            {
+                BrokeredMessage message = queue.Receive();
+                if (message != null)
+                {
+                    try
+                    {
+                        Console.WriteLine("MessageId {0}", message.MessageId);
+                        Console.WriteLine("Delivery {0}", message.DeliveryCount);
+                        Console.WriteLine("Size {0}", message.Size);
+                        Console.WriteLine(message.GetBody<string>()); message.Complete();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        message.Abandon();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Queue Sender/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using System.Threading;
+
+namespace Queue_Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string queueName = "khqueue1";
+            //Copy & paste sender SB Queue endpoint from Azure portal; append TransportType=Amqp to use AMQP
+            string connection = "Endpoint=sb://khtest1.servicebus.windows.net/;SharedAccessKeyName=Sender;SharedAccessKey=Uq3yCwF22HAvxL5RC85tRRWaLCLn7l1xZlAl1lPEuEA=;TransportType=Amqp";
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+            QueueClient queue = factory.CreateQueueClient(queueName);
+            string message = "Queue message over AMQP";
+            BrokeredMessage bm = new BrokeredMessage(message);
+            queue.Send(bm);
+            Console.WriteLine("MessageId {0}", bm.MessageId);
+            Thread.Sleep(3000);
+        }
+    }
+}
